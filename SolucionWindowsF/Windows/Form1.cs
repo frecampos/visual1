a43aba0d2599a2eae187f4e7557e1e8e88f6d59b
freddy campos <fre.camposo@profesor.duoc.cl>
2020-04-14 22:04:19 -0400

    nuevo boton con mensaje

--- SolucionWindowsF/Windows/Form1.cs
+++ SolucionWindowsF/Windows/Form1.cs
@@ -16,5 +16,10 @@ namespace Windows
         {
             InitializeComponent();
         }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hola mundo real");
+        }
     }
 }
